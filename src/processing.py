d365c4f1a8db3d4650cf39f22655a408b3b65267
Iakov <yashatok@mail.ru>
2025-09-04 18:46:04 +0500

    code correction

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,6 @@
-from typing import List, Dict, Any
-from typing import List, Dict, Any
 from datetime import datetime
+from typing import Any, Dict, List
+
 
 def filter_by_state(records: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """
@@ -9,12 +9,15 @@ def filter_by_state(records: List[Dict[str, Any]], state: str = "EXECUTED") -> L
     """
     return [rec for rec in records if rec.get("state") == state]
 
-big_data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-print (filter_by_state(big_data))
+big_data = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+print(filter_by_state(big_data))
 
 
 def sort_by_date(records: List[Dict[str, Any]], descending: bool = True) -> List[Dict[str, Any]]:
@@ -45,9 +48,12 @@ def sort_by_date(records: List[Dict[str, Any]], descending: bool = True) -> List
     # Возвращаем отсортированные записи (только словари) + некорректные в конце
     return [rec for _, rec in valid] + invalid
 
-date = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-print (sort_by_date(date))
+date = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+print(sort_by_date(date))
